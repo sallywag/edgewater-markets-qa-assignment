a19e1efe0e307dd454f346b599fe7c1b2235f94f
salrosa <36641115+salrosa@users.noreply.github.com>
2024-04-17 13:40:10 -0400

    Added spacing to tests to improve readability.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,9 @@ test('Searching for exact book yields correct first result', async ({ page }) =>
   await page.goto('/');
   await page.getByPlaceholder('Search 19 million titles by title, author, or ISBN').fill('The Lord of the Rings: The Fellowship of the Ring by J.R.R. Tolkien');
   await page.locator('.Search-submit').click();
+  
   const firstResult = page.locator('.SearchContentResults-tilesContainer div.AllEditionsItem-tile').first();
+
   await expect(firstResult.locator('.AllEditionsItem-tileTitle')).toHaveText('The Lord of the Rings: The Fellowship of the Ring');
   await expect(firstResult.locator('[itemprop=author]')).toHaveText('J.R.R. Tolkien');
 });
@@ -34,13 +36,18 @@ test('Expected number of items show on search page', async ({ page }) => {
   await page.goto('/');
   await page.getByPlaceholder('Search 19 million titles by title, author, or ISBN').fill('Health');
   await page.locator('.Search-submit').click();
+
   await expect(page.locator('.AllEditionsItem-tile.Recipe-default')).toHaveCount(50);
+
   await page.locator('#Search-sortBar-perpage-description').selectOption('5');
   await expect(page.locator('.AllEditionsItem-tile.Recipe-default')).toHaveCount(5);
+
   await page.locator('#Search-sortBar-perpage-description').selectOption('10');
   await expect(page.locator('.AllEditionsItem-tile.Recipe-default')).toHaveCount(10);
+
   await page.locator('#Search-sortBar-perpage-description').selectOption('30');
   await expect(page.locator('.AllEditionsItem-tile.Recipe-default')).toHaveCount(30);
+
   await page.locator('#Search-sortBar-perpage-description').selectOption('50');
   await expect(page.locator('.AllEditionsItem-tile.Recipe-default')).toHaveCount(50);
 });
