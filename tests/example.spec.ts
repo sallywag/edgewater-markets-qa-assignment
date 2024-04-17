f8a2622720e1198c08a7cbe774c0f71a29b58bca
salrosa <36641115+salrosa@users.noreply.github.com>
2024-04-17 13:00:17 -0400

    Fixed typo in expect.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,7 +6,7 @@ test('Searching for exact book yields correct first result', async ({ page }) =>
   await page.locator('.Search-submit').click();
   const firstResult = page.locator('.SearchContentResults-tilesContainer div.AllEditionsItem-tile').first();
   await expect(firstResult.locator('.AllEditionsItem-tileTitle')).toHaveText('The Lord of the Rings: The Fellowship of the Ring');
-  await expect(firstResult.locator('[itemprop=author]')).toHaveText('J.R.R. sTolkien');
+  await expect(firstResult.locator('[itemprop=author]')).toHaveText('J.R.R. Tolkien');
 });
 
 test('Added book shows in cart', async ({ page }) => {
