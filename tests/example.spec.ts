e3f4270e3658d89382e43a4e343623a36e2d2c96
salrosa <36641115+salrosa@users.noreply.github.com>
2024-04-17 12:19:41 -0400

    Added new test 'Added book shows in cart'.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,4 +7,14 @@ test('Searching for exact book yields correct first result', async ({ page }) =>
   const firstResult = page.locator('.SearchContentResults-tilesContainer div.AllEditionsItem-tile').first();
   await expect(firstResult.locator('.AllEditionsItem-tileTitle')).toHaveText('The Lord of the Rings: The Fellowship of the Ring');
   await expect(firstResult.locator('[itemprop=author]')).toHaveText('J.R.R. sTolkien');
-})
+});
+
+test('Added book shows in cart', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('.BookSlideDesktop-Container').first().click();
+  const bookTitle = await page.locator('.WorkMeta-title').textContent();
+  await page.getByText('Add to cart').click();
+  await page.getByText('View Cart & Checkout').click();
+  await expect(page.locator('.ShoppingCartItem-title')).toHaveText(bookTitle);
+});
+
